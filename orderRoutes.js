1845a028ac8c1f1877f7afe29e44a1a3861d0185
VivekTriveda <vivek@trivedatechnologies.com>
2026-06-13 17:33:46 +0530

    Delete orderRoutes.js

--- orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const Order = require("../models/Order");
-
-router.post("/checkout", async (req, res) => {
-   // checkout code
-});
-
-module.exports = router;
